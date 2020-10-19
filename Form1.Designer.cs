4f66089a52043f58f219a0c24912899484f5c2a8
ERIC FIGUEIREDO <djericmax@hotmail.com>
2020-10-19 03:13:45 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Imagens
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgvMostra = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.ptbImagem = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMostra)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.ptbImagem);
+            this.panel1.Controls.Add(this.dgvMostra);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(528, 259);
+            this.panel1.TabIndex = 0;
+            // 
+            // dgvMostra
+            // 
+            this.dgvMostra.BackgroundColor = System.Drawing.Color.White;
+            this.dgvMostra.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMostra.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dgvMostra.GridColor = System.Drawing.Color.DimGray;
+            this.dgvMostra.Location = new System.Drawing.Point(0, 0);
+            this.dgvMostra.Name = "dgvMostra";
+            this.dgvMostra.Size = new System.Drawing.Size(311, 259);
+            this.dgvMostra.TabIndex = 0;
+            this.dgvMostra.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMostra_CellClick);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnAbrir);
+            this.panel2.Controls.Add(this.btnExcluir);
+            this.panel2.Controls.Add(this.btnSair);
+            this.panel2.Controls.Add(this.btnSalvar);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 259);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(528, 57);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAbrir.Location = new System.Drawing.Point(132, 0);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(132, 57);
+            this.btnAbrir.TabIndex = 1;
+            this.btnAbrir.Text = "&Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnSair.Location = new System.Drawing.Point(396, 0);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(132, 57);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.Text = "Sai&r";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnSalvar.Location = new System.Drawing.Point(0, 0);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(132, 57);
+            this.btnSalvar.TabIndex = 0;
+            this.btnSalvar.Text = "&Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnExcluir.Location = new System.Drawing.Point(264, 0);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(132, 57);
+            this.btnExcluir.TabIndex = 3;
+            this.btnExcluir.Text = "&Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // ptbImagem
+            // 
+            this.ptbImagem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ptbImagem.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ptbImagem.Location = new System.Drawing.Point(311, 0);
+            this.ptbImagem.Name = "ptbImagem";
+            this.ptbImagem.Size = new System.Drawing.Size(217, 259);
+            this.ptbImagem.TabIndex = 1;
+            this.ptbImagem.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 316);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Salvando imagens";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMostra)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImagem)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dgvMostra;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.PictureBox ptbImagem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Imagens
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CarregarGrid();
+        }
+
+        /// <summary>
+        /// método para carregar as informações do banco de dados no grid.
+        /// </summary>
+        private void CarregarGrid()
+        {
+            try
+            {
+                using (var conn = AbrirConexao())
+                {
+                    conn.Open();
+                    using (var comm = conn.CreateCommand())
+                    {
+                        comm.CommandText = "SELECT * FROM imagens";
+                        var reader = comm.ExecuteReader();
+                        var table = new DataTable();
+                        table.Load(reader);
+                        dgvMostra.DataSource = table;
+                        ConfigView();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// método para escolher um arquivo para ser salvo
+        /// </summary>
+        /// <returns>Nome do arquivo a ser salvo.</returns>
+        private string EscolherArquivo()
+        {
+            var retorno = string.Empty;
+            using (var dialog = new OpenFileDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    retorno = dialog.FileName;
+                }
+            }
+            return retorno;
+        }
+
+        /// <summary>
+        /// método para salvar o arquivo novo
+        /// </summary>
+        private void Salvar()
+        {
+            try
+            {
+                var arquivo = EscolherArquivo();
+                if (!string.IsNullOrWhiteSpace(arquivo))
+                {
+                    SalvarArquivo(arquivo);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// método para salvar o arquivo, este executa o processo completo.
+        /// </summary>
+        /// <param name="arquivo"></param>
+        private void SalvarArquivo(string arquivo)
+        {
+            using (var conn = AbrirConexao())
+            {
+                conn.Open();
+                using (var comm = conn.CreateCommand())
+                {
+                    comm.CommandText = "INSERT INTO imagens(nomeArquivo, arquivo)values(@nomeArquivo, @arquivo)";
+                    ConfigurarParametrosSalvar(comm, arquivo);
+                    comm.ExecuteNonQuery();
+                    CarregarGrid();
+                }
+            }
+        }
+
+        /// <summary>
+        /// configura os parâmetros a serem salvos, não afetando quando mudar o banco de dados.
+        /// </summary>
+        /// <param name="comm"></param>
+        /// <param name="arquivo"></param>
+        private void ConfigurarParametrosSalvar(IDbCommand comm, string arquivo)
+        {
+            comm.Parameters.Add(new SqlParameter("nomeArquivo", Path.GetFileName(arquivo)));
+            comm.Parameters.Add(new SqlParameter("arquivo", File.ReadAllBytes(arquivo)));
+        }
+
+        /// <summary>
+        /// método para abrir o arquivo, este executa o processo completo.
+        /// </summary>
+        private void Abrir()
+        {
+            try
+            {
+                using (var conn = AbrirConexao())
+                {
+                    conn.Open();
+                    using (var comm = conn.CreateCommand())
+                    {
+                        comm.CommandText = "select arquivo from imagens where (id = @id)";
+                        ConfigurarParametrosAbrir(comm);
+                        var bytes = comm.ExecuteScalar() as byte[];
+                        if (bytes != null)
+                        {
+                            var nomeArquivo = dgvMostra.CurrentRow.Cells["nomeArquivo"].Value.ToString();
+                            var arquivoTemp = Path.GetTempFileName();
+                            arquivoTemp = Path.ChangeExtension(arquivoTemp, Path.GetExtension(nomeArquivo));
+                            File.WriteAllBytes(arquivoTemp, bytes);
+                            Process.Start(arquivoTemp);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        
+        /// <summary>
+        /// configura os parâmetros para abrir um arquivo existente e salvo no banco de dados, não afeta a alteração de banco.
+        /// </summary>
+        /// <param name="comm"></param>
+        /// <param name="arquivo"></param>
+        private void ConfigurarParametrosAbrir(IDbCommand comm)
+        {
+            comm.Parameters.Add(new SqlParameter("id", dgvMostra.CurrentRow.Cells["id"].Value)); 
+        }
+
+        /// <summary>
+        /// método para iniciar e abrir a conexão. é o endereço do banco de dados.
+        /// </summary>
+        /// <returns></returns>
+        private IDbConnection AbrirConexao()
+        {
+            return new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Eric\Desktop\Imagens\BcoImagens.mdf;Integrated Security=True");
+        }
+
+        private void Excluir()
+        {
+            if (MessageBox.Show("Deseja excluir este registro?","Salvando imagens",MessageBoxButtons.YesNo)==DialogResult.Yes)
+            {
+                ExcluirArquivo();
+            }
+        }
+
+        private void ExcluirArquivo()
+        {
+            try
+            {
+                using (var conn = AbrirConexao())
+                {
+                    conn.Open();
+                    using (var comm = conn.CreateCommand())
+                    {
+                        comm.CommandText = "delete from imagens where (id = @id)";
+                        ConfigurarParametrosExcluir(comm);
+                        comm.ExecuteNonQuery();
+                        CarregarGrid();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ConfigurarParametrosExcluir(IDbCommand comm)
+        {
+            comm.Parameters.Add(new SqlParameter("id", dgvMostra.CurrentRow.Cells["id"].Value));
+        }
+
+        /// <summary>
+        /// botão SALVAR
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Salvar();
+        }
+
+        /// <summary>
+        /// Botão Abrir
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            Abrir();
+        }
+
+        /// <summary>
+        /// Botão Sair
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja sair da aplicação","Salvando imagens",MessageBoxButtons.YesNo)==DialogResult.Yes)
+            {
+                Application.Exit();
+            }            
+        }
+
+        /// <summary>
+        /// Botão excluir.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            Excluir();
+        }
+
+        /// <summary>
+        /// Configurar a largura e o cabeçalho das colunas.
+        /// </summary>
+        public void ConfigView()
+        {
+            dgvMostra.Columns[0].HeaderText = "ID";
+            dgvMostra.Columns[1].HeaderText = "Nome da Imagem";
+            dgvMostra.Columns[0].Width = 35;
+            dgvMostra.Columns[1].Width = 230;
+            dgvMostra.Columns[2].Visible= false;
+        }
+
+        public void MudarImgGrd()
+        {
+            try
+            {
+                MemoryStream image = new MemoryStream((byte[])dgvMostra.CurrentRow.Cells[2].Value‌​);
+                ptbImagem.BackgroundImage = System.Drawing.Image.FromStream(image);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void dgvMostra_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            MudarImgGrd();
+        }
+    }
+}
